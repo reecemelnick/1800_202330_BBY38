7e2c519911e2e84ee8bef4c8b58435c394e9a97a
John Guo <johnguoyh@gmail.com>
2023-11-14 10:19:09 -0800

    fixed login, made it so when login get sent to main page after

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -22,7 +22,8 @@ var uiConfig = {
               console.log("Error adding new user: " + error);
           });
       } else {
-          return true;
+          //console.log("bruh");
+          window.location.assign("main.html");
       }
       return false;
   },
