a2114b2c22826e26de6bc1837943a748b2e0531d
John Guo <johnguoyh@gmail.com>
2023-11-18 22:28:17 -0800

    made stupid bookmark icon align with the bottom

--- scripts/bookmark.js
+++ scripts/bookmark.js
@@ -31,46 +31,63 @@ function getBookmarks(user) {
             console.log(currentUser);
         }
     })
-    
+
     db.collection("users").doc(user.uid).get()
         .then(userDoc => {
 
             var bookmarks = userDoc.data().bookmarks;
             console.log(bookmarks);
 
-            if(bookmarks.length != 0) {
+            if (bookmarks.length != 0) {
                 bookmarks.forEach(thisListingID => {
                     console.log(thisListingID);
-    
+
                     db.collection("listings").doc(thisListingID).get().then(doc => {
-    
+
                         var title = doc.data().name;
                         var details = doc.data().details;
+                        var desc = doc.data().description;
+                        var type = doc.data().type;
+                        var edi = doc.data().edition;
+                        var condi = doc.data().condition;
+                        var timeStamp = doc.data().timestamp;
                         var listingCode = doc.data().code;
                         var listingPrice = doc.data().price;
                         var docID = doc.id;
                         let newcard = listingTemplate.content.cloneNode(true);
-    
+
                         newcard.querySelector('.card-title').innerHTML = title;
                         newcard.querySelector('.card-price').innerHTML = "$" + listingPrice;
                         newcard.querySelector('.card-text').innerHTML = details;
+
+                        newcard.querySelector('.card-text').innerHTML = desc;
+                        newcard.querySelector('.card-type').innerHTML = type;
+                        newcard.querySelector('.card-edition').innerHTML = "Edition: " + edi;
+                        newcard.querySelector('.card-condition').innerHTML = "Condition: " + condi;
+
+                        if (timeStamp) {
+                            newcard.querySelector('.card-time').innerHTML = timeStamp.toDate();
+                        } else {
+                            newcard.querySelector('.card-time').innerHTML = "Timestamp not available";
+                        }
+
                         newcard.querySelector('.card-image').src = `./images/${listingCode}.jpg`;
-    
+
                         newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique
-    
+
                         console.log();
                         document.getElementById("listings-go-here").appendChild(newcard);
-    
-    
+
+
                         currentUser.get().then(userDoc => {
                             //get the user name
                             var bookmarks = userDoc.data().bookmarks;
                             if (bookmarks.includes(docID)) {
                                 document.getElementById('save-' + docID).innerText = 'bookmark';
                             }
-    
+
                         })
-    
+
                     });
                 })
             } else {
