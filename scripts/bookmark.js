db94984d77c6353d33bc2c488598be4ef807eba9
John Guo <johnguoyh@gmail.com>
2023-11-18 22:09:12 -0800

    made the bookmarks work, although admitily the sorting feature doesn't work yet

---
+++ scripts/bookmark.js
@@ -0,0 +1,87 @@
+function doAll() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            //insertNameFromFirestore(user);
+            getBookmarks(user)
+        } else {
+            console.log("No user is signed in");
+        }
+    });
+}
+
+doAll();
+
+
+function insertNameFromFirestore(user) {
+    db.collection("users").doc(user.uid).get().then(userDoc => {
+        console.log(userDoc.data().name)
+        userName = userDoc.data().name;
+        console.log(userName)
+        document.getElementById("name-goes-here").innerHTML = userName;
+    })
+
+}
+
+var currentUser;
+function getBookmarks(user) {
+    let listingTemplate = document.getElementById("listingCardTemplate");
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
+            console.log(currentUser);
+        }
+    })
+    
+    db.collection("users").doc(user.uid).get()
+        .then(userDoc => {
+
+            var bookmarks = userDoc.data().bookmarks;
+            console.log(bookmarks);
+
+            if(bookmarks.length != 0) {
+                bookmarks.forEach(thisListingID => {
+                    console.log(thisListingID);
+    
+                    db.collection("listings").doc(thisListingID).get().then(doc => {
+    
+                        var title = doc.data().name;
+                        var details = doc.data().details;
+                        var listingCode = doc.data().code;
+                        var listingPrice = doc.data().price;
+                        var docID = doc.id;
+                        let newcard = listingTemplate.content.cloneNode(true);
+    
+                        newcard.querySelector('.card-title').innerHTML = title;
+                        newcard.querySelector('.card-price').innerHTML = "$" + listingPrice;
+                        newcard.querySelector('.card-text').innerHTML = details;
+                        newcard.querySelector('.card-image').src = `./images/${listingCode}.jpg`;
+    
+                        newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique
+    
+                        console.log();
+                        document.getElementById("listings-go-here").appendChild(newcard);
+    
+    
+                        currentUser.get().then(userDoc => {
+                            //get the user name
+                            var bookmarks = userDoc.data().bookmarks;
+                            if (bookmarks.includes(docID)) {
+                                document.getElementById('save-' + docID).innerText = 'bookmark';
+                            }
+    
+                        })
+    
+                    });
+                })
+            } else {
+                document.getElementById("listings-go-here").innerText("noBookmarks");
+
+            }
+
+        })
+        .catch(error => {
+            console.error("Error getting documents from Firestore:", error);
+        });
+}
+
+
--- scripts/listings.js
+++ scripts/listings.js
@@ -11,17 +11,28 @@ function addListing() {
         price: "18,000",
         details: "Good",
     })
-    .then((docRef) => {
-        console.log('Document written with ID:', docRef.id);
-    })
-    .catch((error) => {
-        console.error('Error adding document:', error);
-    });
+        .then((docRef) => {
+            console.log('Document written with ID:', docRef.id);
+        })
+        .catch((error) => {
+            console.error('Error adding document:', error);
+        });
 }
+
+
+
+var currentUser;
 
 
 function displayListingsDynamically(collection) {
     let listingTemplate = document.getElementById("listingCardTemplate");
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
+            console.log(currentUser);
+        }
+    })
 
     db.collection(collection).get()
         .then(allListings => {
@@ -30,6 +41,7 @@ function displayListingsDynamically(collection) {
                 var details = doc.data().details;
                 var listingCode = doc.data().code;
                 var listingPrice = doc.data().price;
+                var docID = doc.id;
                 let newcard = listingTemplate.content.cloneNode(true);
 
                 newcard.querySelector('.card-title').innerHTML = title;
@@ -37,7 +49,22 @@ function displayListingsDynamically(collection) {
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${listingCode}.jpg`;
 
+                newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique
+
+                newcard.querySelector('i').onclick = () => saveBookmark(docID);
+
                 document.getElementById(collection + "-go-here").appendChild(newcard);
+
+
+                currentUser.get().then(userDoc => {
+                    //get the user name
+                    var bookmarks = userDoc.data().bookmarks;
+                    if (bookmarks.includes(docID)) {
+                        document.getElementById('save-' + docID).innerText = 'bookmark';
+                    }
+
+                })
+
             });
         })
         .catch(error => {
@@ -48,3 +75,21 @@ function displayListingsDynamically(collection) {
 // Example: Call the function with the "listings" collection
 displayListingsDynamically("listings");
 
+
+
+function saveBookmark(listingDocID) {
+    // Manage the backend process to store the hikeDocID in the database, recording which hike was bookmarked by the user.
+    currentUser.update({
+        // Use 'arrayUnion' to add the new bookmark ID to the 'bookmarks' array.
+        // This method ensures that the ID is added only if it's not already present, preventing duplicates.
+        bookmarks: firebase.firestore.FieldValue.arrayUnion(listingDocID)
+    })
+        // Handle the front-end update to change the icon, providing visual feedback to the user that it has been clicked.
+        .then(function () {
+            console.log("bookmark has been saved for" + listingDocID);
+            var iconID = 'save-' + listingDocID;
+            //console.log(iconID);
+            //this is to change the icon of the hike that was saved to "filled"
+            document.getElementById(iconID).innerText = 'bookmark';
+        });
+}
