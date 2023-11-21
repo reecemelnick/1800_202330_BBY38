b9466e76707019e22c344297f7b3ba7703466d6c
reecemelnick <reececianmelnick@gmail.com>
2023-11-21 09:27:53 -0800

    Footer update

--- scripts/makeListing.js
+++ scripts/makeListing.js
@@ -31,4 +31,22 @@ function makeListing() {
     // }
 }
 
+function chooseFileListener() {
+    const fileInput = document.getElementById("mypic-input");   // pointer #1
+    const image = document.getElementById("mypic-goes-here");   // pointer #2
+
+    //attach listener to input file
+    //when this file changes, do something
+    fileInput.addEventListener('change', function (e) {
+
+        //the change event returns a file "e.target.files[0]"
+        ImageFile = e.target.files[0];
+        var blob = URL.createObjectURL(ImageFile);
+
+        //change the DOM img element source to point to this file
+        image.src = blob;    //assign the "src" property of the "img" tag
+    })
+}
+chooseFileListener();
+
 
