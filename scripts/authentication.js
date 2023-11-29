05ddc4f95029a58a9dae6d9a1a5dbcc8e494211b
John Guo <johnguoyh@gmail.com>
2023-11-29 15:08:16 -0800

    show my listings under profile page

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,28 +1,26 @@
-// Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
      
-      var user = authResult.user;                            // get the user object from the Firebase authentication database
-      if (authResult.additionalUserInfo.isNewUser) {         //if new user
-          db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-              name: user.displayName,                    //"users" collection
-              email: user.email,                         //with authenticated user's ID (user.uid)
-              country: "Canada",                      //optional default profile info      
+      var user = authResult.user;                            
+      if (authResult.additionalUserInfo.isNewUser) {         
+          db.collection("users").doc(user.uid).set({         
+              name: user.displayName,                    
+              email: user.email,                         
+              country: "Canada",                      
               city: "Burnaby",
               address: "",
               interests: ""
 
           }).then(function () {
               console.log("New user added to firestore");
-              window.location.assign("createProfile.html");       //re-direct to main.html after signup
+              window.location.assign("createProfile.html");       
           }).catch(function (error) {
               console.log("Error adding new user: " + error);
           });
       } else {
-          //console.log("bruh");
           window.location.assign("main.html");
       }
       return false;
